d1df32001543790d07b2eda08c1394174def2a3c
Yashwanth Reddy <yashwanthvenati@gmail.com>
2019-10-23 18:02:00 -0500

    Init App
    Sample App to try React Navigation

---
+++ src/components/About.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => {
+  return <h2>My name is Yash and I am a Full Stack Data Scientist.</h2>;
+};
+
+export default About;
---
+++ src/components/Resume.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Resume = () => {
+  return (
+    <div>
+      <h1>Yashwanth Reddy Venati </h1>
+      <h2>Masters in Computer Science</h2>
+      <h3>University of Houston</h3>
+      <h2>Work History</h2>
+      <h3>Data Scientist</h3>
+      <ul>
+        <li>JOB DESCRIPTION 2</li>
+        <li>JOB DESCRIPTION 1</li>
+      </ul>
+      <h2>Skills</h2>
+      <ul>
+        <li>React.js</li>
+        <li>React Router</li>
+        <li>Node.js</li>
+        <li>Tensorflow</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Resume;
